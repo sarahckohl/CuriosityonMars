106c936984c854be69d7bb6788f41e6348b3bc56
x3jasmine <x3_momoko@hotmail.com>
2014-05-07 01:51:28 -0700

    Mock back ground
    just added a mock background under the grid so you could see what you're
    doing. When you run the game, it creates a border for the 8x6 map

--- CuriosityOnMars/Assets/Standard Assets/Scripts/General Scripts/Grid.cs
+++ CuriosityOnMars/Assets/Standard Assets/Scripts/General Scripts/Grid.cs
@@ -4,21 +4,32 @@ using System.Collections;
 public class Grid : MonoBehaviour {
 
 	public GameObject sprite;
+	public GameObject border;
 	public int width = 10;
 	public int height = 10;
 
 	private GameObject [,] grid = new GameObject[10,10];
+	GameObject gridPlane;
 
 	void Awake() {
 		//cols
 		for (int i = 0; i < width; i++) {
 			//rows
 			for (int j = 0; j < height; j++) {
-				GameObject gridPlane = (GameObject)Instantiate(sprite);
-				gridPlane.transform.position = new Vector3 (gridPlane.transform.position.x+i, 
-				                                            gridPlane.transform.position.y+j, 
-				                                            0);
-				grid[i,j] = gridPlane;
+				if (j==height-1 || j== 2 || i == 0 || i == width-1){
+					gridPlane = (GameObject)Instantiate(border);
+					gridPlane.transform.position = new Vector3 (gridPlane.transform.position.x+i, 
+					                                            gridPlane.transform.position.y+j, 
+					                                            0);
+					grid[i,j] = gridPlane;
+				}
+				else {
+					gridPlane = (GameObject)Instantiate(sprite);
+					gridPlane.transform.position = new Vector3 (gridPlane.transform.position.x+i, 
+				                                            	gridPlane.transform.position.y+j, 
+				                                            	0);
+					grid[i,j] = gridPlane;
+				}
 			}
 		}
 	}
