73c76af05d5fba2a70c261fab344df6c2081786e
amcolema <amcolema@ucsc.edu>
2014-05-07 22:12:39 -0700

    Rover + Impellers
    Rover has 4 directions. Rover moves once a second. Impellers will change
    the rover's direction.

---
+++ CuriosityOnMars/Assets/control_right_impeller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control_right_impeller : MonoBehaviour {
+
+	public control_rover rover;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// see if the rover is at the same coordinates as this
+		// if it is, set its direction to right
+		if(rover.transform.position.x.Equals(this.transform.position.x) &&
+		   rover.transform.position.y.Equals(this.transform.position.y))
+			rover.dir = control_rover.Direction.Right;
+	}
+}
