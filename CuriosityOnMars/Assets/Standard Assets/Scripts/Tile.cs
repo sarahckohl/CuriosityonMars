1bf08f724f0d3109c8831ae16832639974e0939b
Sarah Kohl <sarahcharlottekohl@gmail.com>
2014-05-11 01:18:28 -0700

    Redid the stuff lost when my issues started

---
+++ CuriosityOnMars/Assets/Standard Assets/Scripts/Tile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.renderer.material.color = Color.clear;	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
