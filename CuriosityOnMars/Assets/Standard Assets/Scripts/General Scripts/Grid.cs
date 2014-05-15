04df6a71a7220c1bfaf8187e7c657c38e735cc96
x3jasmine <x3_momoko@hotmail.com>
2014-05-15 00:29:07 -0700

    Destructor
    Doesn't reset the level though. Not sure why.

--- CuriosityOnMars/Assets/Standard Assets/Scripts/General Scripts/Grid.cs
+++ CuriosityOnMars/Assets/Standard Assets/Scripts/General Scripts/Grid.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Grid : MonoBehaviour {
 
 	public GameObject sprite;
-	public GameObject border;
+	//public GameObject border;
 	private int width = 12;
 	private int height = 11;
 
@@ -16,12 +16,12 @@ public class Grid : MonoBehaviour {
 		for (int i = 0; i < width; i++) {
 			//rows
 			for (int j = 0; j < height; j++) {
-				if (j==height-1 || j== 1 || i == 0 || i == width-1){
-					gridPlane = (GameObject)Instantiate(border);
-				}
-				else {
+				//if (j==height-1 || j== 1 || i == 0 || i == width-1){
+				//	gridPlane = (GameObject)Instantiate(border);
+				//}
+				//else {
 					gridPlane = (GameObject)Instantiate(sprite);
-				}
+				//}
 				gridPlane.transform.position = new Vector3 (gridPlane.transform.position.x+i, 
 				                                            gridPlane.transform.position.y+j, 
 				                                            0);
